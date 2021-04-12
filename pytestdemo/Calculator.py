7abacaa0375dc95c4457231c59cfae970be3c586
zhangchao-plus <ilove199212@163.com>
2021-04-12 22:14:20 +0800

    add：设计一个计算器功能，实现加法和除法运算

---
+++ pytestdemo/Calculator.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: 笨笨笨啊啊啊
+# Date  : 2021/04/12 下午 09:55
+# File  : Calculator.py
+
+# 计算器功能
+class Calculator():
+    # 加法运算
+    def add(self, a, b):
+        return a + b
+
+    # 除法运算
+    def div(self, a, b):
+        return a / b
