af9fa76d3b70dc0d694986a29068d053e2c83729
zhangchao-plus <ilove199212@163.com>
2021-04-12 19:59:48 +0800

    add：新建一个pytest例子

---
+++ test_example.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: 笨笨笨啊啊啊
+# Date  : 2021/04/12 下午 04:27
+# File  : test_example.py
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 4
