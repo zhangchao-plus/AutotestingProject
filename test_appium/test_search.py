b8a8c7ff79ac4d3794ea7518a2baa20177e0fae9
zhangchao-plus <ilove199212@163.com>
2021-05-19 23:17:54 +0800

    modify:练习使用xpath和uiautomator定位

--- test_appium/test_search.py
+++ test_appium/test_search.py
@@ -50,3 +50,44 @@ class TestAppium():
         y_end = int(height * 1/5)
         action.press(x=x1, y=y_start).wait(1000).move_to(x=x1, y=y_end).release().perform()
 
+    def test_get_price(self):
+        """
+        使用Xpath节点获取阿里巴巴-sw的股价
+        """
+        self.driver.find_element_by_id("com.xueqiu.android:id/home_search").click()
+        self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("阿里巴巴")
+        self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name' and @text ='阿里巴巴']").click()
+        current_price = float(self.driver.find_element_by_xpath(
+            "//*[@text='09988']/../../..//*[@resource-id='com.xueqiu.android:id/current_price']").text)
+        print(current_price)
+        assert current_price >= 200
+
+    def test_login(self):
+        """
+        UiSelector()定位：
+        1、点击我的，进入用户信息页面；
+        2、点击账号密码登录，进入登录页面；
+        3、输入账号，密码，点击登录；
+        """
+        # 使用resourceId和text组合定位，并点击我的
+        loc_my = 'new UiSelector().resourceId("com.xueqiu.android:id/tab_name").text("我的")'
+        self.driver.find_element_by_android_uiautomator(loc_my).click()
+        # 使用text定位，并点击帐号密码登录
+        loc_login = 'new UiSelector().text("帐号密码登录")'
+        self.driver.find_element_by_android_uiautomator(loc_login).click()
+        # 输入账号
+        loc_account = 'new UiSelector().resourceId("com.xueqiu.android:id/login_account")'
+        self.driver.find_element_by_android_uiautomator(loc_account).send_keys("18702888141")
+        # 输入密码
+        loc_password = 'new UiSelector().resourceId("com.xueqiu.android:id/login_password")'
+        self.driver.find_element_by_android_uiautomator(loc_password).send_keys("abc@1234")
+        # 点击登录
+        loc_button = 'new UiSelector().resourceId("com.xueqiu.android:id/button_next")'
+        self.driver.find_element_by_android_uiautomator(loc_button).click()
+        # 获取登录失败文本
+        login_error_text = self.driver.find_element_by_id("com.xueqiu.android:id/md_content").text
+        print(login_error_text)
+
+
+
+
